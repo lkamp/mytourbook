44372d1edc76df9de59763000be9628a93dfe969
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2016-10-20 18:07:23 +0200

    fixed mouse wheel was not working at all

--- bundles/net.tourbook/src/net/tourbook/map2mf/view/Map2mfView.java
+++ bundles/net.tourbook/src/net/tourbook/map2mf/view/Map2mfView.java
@@ -15,10 +15,17 @@
  *******************************************************************************/
 package net.tourbook.map2mf.view;
 
+import java.awt.Component;
+import java.awt.EventQueue;
 import java.awt.Frame;
+import java.awt.Point;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
 import java.io.File;
+
+import javax.swing.SwingUtilities;
 
 import net.tourbook.application.TourbookPlugin;
 
@@ -27,12 +34,12 @@ import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.awt.SWT_AWT;
+import org.eclipse.swt.events.MouseWheelListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 import org.mapsforge.core.graphics.GraphicFactory;
 import org.mapsforge.core.model.BoundingBox;
 import org.mapsforge.core.model.LatLong;
-import org.mapsforge.core.model.Point;
 import org.mapsforge.core.util.LatLongUtils;
 import org.mapsforge.map.awt.graphics.AwtGraphicFactory;
 import org.mapsforge.map.awt.view.MapView;
@@ -41,8 +48,6 @@ import org.mapsforge.map.layer.cache.FileSystemTileCache;
 import org.mapsforge.map.layer.cache.InMemoryTileCache;
 import org.mapsforge.map.layer.cache.TileCache;
 import org.mapsforge.map.layer.cache.TwoLevelTileCache;
-import org.mapsforge.map.layer.debug.TileCoordinatesLayer;
-import org.mapsforge.map.layer.debug.TileGridLayer;
 import org.mapsforge.map.layer.download.TileDownloadLayer;
 import org.mapsforge.map.layer.download.tilesource.OpenStreetMapMapnik;
 import org.mapsforge.map.layer.download.tilesource.TileSource;
@@ -114,8 +119,8 @@ public class Map2mfView extends ViewPart {
 
 		// Debug
 		if (SHOW_DEBUG_LAYERS) {
-			layers.add(new TileGridLayer(GRAPHIC_FACTORY, mapView.getModel().displayModel));
-			layers.add(new TileCoordinatesLayer(GRAPHIC_FACTORY, mapView.getModel().displayModel));
+//			layers.add(new TileGridLayer(GRAPHIC_FACTORY, mapView.getModel().displayModel));
+//			layers.add(new TileCoordinatesLayer(GRAPHIC_FACTORY, mapView.getModel().displayModel));
 		}
 
 		return result;
@@ -161,9 +166,12 @@ public class Map2mfView extends ViewPart {
 				GRAPHIC_FACTORY) {
 
 			@Override
-			public boolean onTap(final LatLong tapLatLong, final Point layerXY, final Point tapXY) {
+			public boolean onTap(	final LatLong tapLatLong,
+									final org.mapsforge.core.model.Point layerXY,
+									final org.mapsforge.core.model.Point tapXY) {
 
 				System.out.println("Tap on: " + tapLatLong);
+
 				return true;
 			}
 		};
@@ -179,7 +187,7 @@ public class Map2mfView extends ViewPart {
 		 * be suitable for your application.
 		 */
 		try {
-			System.setProperty("sun.awt.noerasebackground", "true");
+//			System.setProperty("sun.awt.noerasebackground", "true");
 		} catch (final NoSuchMethodError error) {}
 
 		// Increase read buffer limit
@@ -193,12 +201,12 @@ public class Map2mfView extends ViewPart {
 
 		GridDataFactory.fillDefaults().grab(true, true).applyTo(parent);
 
-		// build GUI: container(SWT) -> Frame(AWT) -> Panel(AWT) -> WorldWindowGLCanvas(AWT)
 		_mapContainer = new Composite(parent, SWT.EMBEDDED | SWT.NO_BACKGROUND);
-//		GridDataFactory.fillDefaults().applyTo(_mapContainer);
 		{
 			_awtFrame = SWT_AWT.new_Frame(_mapContainer);
 			_awtFrame.add(mapView);
+
+			setup_SWT_AWT_MouseWheelListener(_mapContainer, _awtFrame);
 		}
 
 		_awtFrame.addComponentListener(new ComponentAdapter() {
@@ -221,5 +229,113 @@ public class Map2mfView extends ViewPart {
 	@Override
 	public void setFocus() {
 
+	}
+
+	/**
+	 * Set SWT->AWT mouse wheel listener, otherwise mouse wheel is not recognized, found in www.
+	 * 
+	 * @param composite
+	 * @param awtFrame
+	 */
+	private void setup_SWT_AWT_MouseWheelListener(final Composite composite, final Frame awtFrame) {
+//
+//		// (A) force focus when AWT childs receive mouse events
+//		final java.awt.event.MouseListener mlAwt = new java.awt.event.MouseListener() {
+//
+//			@Override
+//			public void mouseClicked(final MouseEvent e) {}
+//
+//			@Override
+//			public void mouseEntered(final MouseEvent e) {
+//				System.err.println("force focus from AWT entered \t\t" + e);
+//				composite.getDisplay().asyncExec(new Runnable() {
+//					@Override
+//					public void run() {
+//						composite.forceFocus();
+//					}
+//				});
+//			}
+//
+//			@Override
+//			public void mouseExited(final MouseEvent e) {}
+//
+//			@Override
+//			public void mousePressed(final MouseEvent e) {
+//				System.err.println("force focus from AWT entered \t\t" + e);
+//				composite.getDisplay().asyncExec(new Runnable() { // (shift to SWT thread)
+//					@Override
+//					public void run() {
+//						composite.forceFocus();
+//					}
+//				});
+//			}
+//
+//			@Override
+//			public void mouseReleased(final MouseEvent e) {}
+//		};
+//
+//		final java.awt.event.MouseMotionListener mlAwt2 = new MouseMotionListener() {
+//
+//			@Override
+//			public void mouseDragged(final MouseEvent e) {}
+//
+//			@Override
+//			public void mouseMoved(final MouseEvent e) {
+//				System.err.println("force focus from AWT entered \t\t" + e);
+//				composite.getDisplay().asyncExec(new Runnable() {
+//					@Override
+//					public void run() {
+//						composite.forceFocus();
+//					}
+//				});
+//			}
+//		};
+
+		// (B) forward messages from SWT to AWT
+		composite.addMouseWheelListener(new MouseWheelListener() {
+
+			@Override
+			public void mouseScrolled(final org.eclipse.swt.events.MouseEvent event) {
+
+//				System.out.println("SWT event " + event);
+
+				// (shift to AWT thread)
+				EventQueue.invokeLater(new Runnable() {
+					@Override
+					public void run() {
+
+						final Component c = SwingUtilities.getDeepestComponentAt(awtFrame, event.x, event.y);
+						if (c != null) {
+
+							final Point bp = awtFrame.getLocationOnScreen();
+							final Point cp = c.getLocationOnScreen();
+
+							final int scrollAmount = event.count > 0 ? -1 : 1;
+
+							final MouseEvent e = new MouseWheelEvent(
+									c,
+									MouseEvent.MOUSE_WHEEL,
+									event.time & 0xFFFFFFFFL,
+									0, // modifiers
+									event.x - (cp.x - bp.x),
+									event.y - (cp.y - bp.y),
+									0, // click count
+									false,
+									MouseWheelEvent.WHEEL_UNIT_SCROLL,
+									scrollAmount,
+									scrollAmount);
+
+//							System.out.println("AWT event " + e);
+
+							c.dispatchEvent(e);
+						}
+					}
+				});
+			}
+		});
+//
+//		// remind to add the awt mouse listener to all the children to be added	into the AWT frame:
+//		// <childrenAwtComponent>.addMouseListener(mlAwt);
+//		// <childrenAwtComponent>.addMouseMotionListener(mlAwt2);
 	}
 }
